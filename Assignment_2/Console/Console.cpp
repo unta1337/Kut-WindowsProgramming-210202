ae7a9c978704870a9448b1fe6565d4d1070f0cdb
S.Y. Kim <firstapple42@gmail.com>
2021-10-08 03:23:09 +0900

    Assignment_2 Console 추가

---
+++ Assignment_2/Console/Console.cpp
@@ -0,0 +1,43 @@
+﻿#include "pch.h"
+#include "framework.h"
+#include "Console.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+CWinApp theApp;
+
+using namespace std;
+
+int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
+{
+    int nRetCode = 0;
+
+    HMODULE hModule = ::GetModuleHandle(NULL);
+
+    if (hModule != NULL)
+    {
+        // MFC를 초기화합니다. 초기화하지 못한 경우 오류를 인쇄합니다.
+        if (!AfxWinInit(hModule, NULL, ::GetCommandLine(), 0))
+        {
+            // TODO: 여기에 애플리케이션 동작을 코딩합니다.
+            _tprintf(L"심각한 오류: MFC 초기화 실패\n");
+            nRetCode = 1;
+        }
+        else
+        {
+            CString str;
+            str.LoadString(IDS_APP_TITLE);
+            _tprintf(_T("Hello, from %s!\n"), str);
+        }
+    }
+    else
+    {
+        // TODO: 오류 코드를 필요에 따라 수정합니다.
+        _tprintf(L"심각한 오류: GetModuleHandle 실패\n");
+        nRetCode = 1;
+    }
+
+    return nRetCode;
+}
