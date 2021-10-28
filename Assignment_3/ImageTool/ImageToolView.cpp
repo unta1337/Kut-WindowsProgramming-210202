0f603db6a3b8d57c70ab821dce329a4ca01c8b20
S.Y. Kim <firstapple42@gmail.com>
2021-10-28 20:36:37 +0900

    과제 3 ImageTool 추가

---
+++ Assignment_3/ImageTool/ImageToolView.cpp
@@ -0,0 +1,105 @@
+﻿
+// ImageToolView.cpp: CImageToolView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "ImageTool.h"
+#endif
+
+#include "ImageToolDoc.h"
+#include "ImageToolView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CImageToolView
+
+IMPLEMENT_DYNCREATE(CImageToolView, CView)
+
+BEGIN_MESSAGE_MAP(CImageToolView, CView)
+	// 표준 인쇄 명령입니다.
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
+END_MESSAGE_MAP()
+
+// CImageToolView 생성/소멸
+
+CImageToolView::CImageToolView() noexcept
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+
+}
+
+CImageToolView::~CImageToolView()
+{
+}
+
+BOOL CImageToolView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: CREATESTRUCT cs를 수정하여 여기에서
+	//  Window 클래스 또는 스타일을 수정합니다.
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CImageToolView 그리기
+
+void CImageToolView::OnDraw(CDC* /*pDC*/)
+{
+	CImageToolDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 여기에 원시 데이터에 대한 그리기 코드를 추가합니다.
+}
+
+
+// CImageToolView 인쇄
+
+BOOL CImageToolView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 기본적인 준비
+	return DoPreparePrinting(pInfo);
+}
+
+void CImageToolView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄하기 전에 추가 초기화 작업을 추가합니다.
+}
+
+void CImageToolView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄 후 정리 작업을 추가합니다.
+}
+
+
+// CImageToolView 진단
+
+#ifdef _DEBUG
+void CImageToolView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CImageToolView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CImageToolDoc* CImageToolView::GetDocument() const // 디버그되지 않은 버전은 인라인으로 지정됩니다.
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CImageToolDoc)));
+	return (CImageToolDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CImageToolView 메시지 처리기
