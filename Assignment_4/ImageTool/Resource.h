25e20aac281f9e61daa82d7c1c2b3273fef018dd
S.Y. Kim <firstapple42@gmail.com>
2021-11-04 21:00:53 +0900

    과제 4: 이론, 대화 상자 추가

--- Assignment_4/ImageTool/Resource.h
+++ Assignment_4/ImageTool/Resource.h
@@ -1,20 +1,24 @@
 ﻿//{{NO_DEPENDENCIES}}
 // Microsoft Visual C++에서 생성한 포함 파일입니다.
-// ImageTool.rc에서 사용
+// ImageTool.rc에서 사용되고 있습니다.
 //
-#define IDD_ABOUTBOX				100
-#define IDP_OLE_INIT_FAILED			100
-#define IDR_MAINFRAME				128
-#define IDR_ImageToolTYPE				130
-#define ID_WINDOW_MANAGER			131
+#define IDD_FILE_NEW                    1
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDR_MAINFRAME                   128
+#define IDR_ImageToolTYPE               130
+#define ID_WINDOW_MANAGER               131
+#define IDC_WIDTH                       1000
+#define IDC_HEIGHT                      1001
+#define IDC_IMAGE_TYPE                  1002
 
-// 다음은 새 개체에 사용할 기본값입니다.
-//
+// Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE	310
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		310
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        314
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_SYMED_VALUE           310
 #endif
 #endif
