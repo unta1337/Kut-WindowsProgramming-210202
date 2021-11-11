6f9186f9789b61f850a645540351ed76f693b954
unta1337 <firstapple42@gmail.com>
2021-11-11 09:55:56 +0900

    실습주 2차시 4: 메타파일 그리기 추가

--- _w/_4/ChildView.cpp
+++ _w/_4/ChildView.cpp
@@ -16,15 +16,29 @@
 
 CChildView::CChildView()
 {
+	// 확장 메타파일 생성 및 초기화.
+	CMetaFileDC dc;
+	dc.CreateEnhanced(NULL, NULL, NULL, NULL);
+
+	// 멤버 변수 호출 및 출력.
+	dc.Rectangle(0, 0, 10, 10);
+	dc.Ellipse(1, 1, 9, 9);
+
+	// 확장 메타파일 핸들을 얻는다.
+	m_hmf = dc.CloseEnhanced();
 }
 
 CChildView::~CChildView()
 {
+	// 확장 메타파일을 삭제한다.
+	::DeleteEnhMetaFile(m_hmf);
 }
 
 
 BEGIN_MESSAGE_MAP(CChildView, CWnd)
 	ON_WM_PAINT()
+	ON_WM_LBUTTONDOWN()
+	ON_WM_RBUTTONDOWN()
 END_MESSAGE_MAP()
 
 
@@ -53,3 +67,19 @@ void CChildView::OnPaint()
 	// 그리기 메시지에 대해서는 CWnd::OnPaint()를 호출하지 마십시오.
 }
 
+
+
+void CChildView::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	CClientDC dc(this);
+	CRect rect(point.x - 20, point.y - 20, point.x + 20, point.y + 20);
+	dc.PlayMetaFile(m_hmf, &rect);
+}
+
+
+void CChildView::OnRButtonDown(UINT nFlags, CPoint point)
+{
+	CClientDC dc(this);
+	CRect rect(point.x - 50, point.y - 20, point.x + 50, point.y + 20);
+	dc.PlayMetaFile(m_hmf, &rect);
+}
